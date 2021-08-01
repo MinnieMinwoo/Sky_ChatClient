5624e324876b33fb9fd37b75d643b8e964b6d4c4
MinnieMinwoo <nhs075241@gmail.com>
2021-08-01 17:09:02 +0900

    asdf

--- Sky_ChatClient/ClientData.cs
+++ Sky_ChatClient/ClientData.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace Sky_ChatClient
 {
     class ClientData
     {
+        public TcpClient client; //tcpclient
         public string clientName; //이름
         public string ipAdress; //ip주소
         public string testType; //학생 or 감독관
         public string chattingRoom; //채팅방 이름
+        public byte[] messageData; //메세지 데이터를 담을 버퍼
 
         //정보를 입력받습니다. 채팅방은 이후 서버에서 값을 돌려받게 됩니다.
         public ClientData()
@@ -20,7 +23,8 @@ namespace Sky_ChatClient
             clientName = Console.ReadLine();
             Console.WriteLine("학생이면 1, 감독관이면 2를 입력하세요");
             testType = Console.ReadLine();
-            chattingRoom = null;
+            messageData = new byte[1024];
+            chattingRoom = "1";
         }
     }
 }
--- Sky_ChatClient/ClientManager.cs
+++ Sky_ChatClient/ClientManager.cs
@@ -18,6 +18,10 @@ namespace Sky_ChatClient
             clientData = new ClientData();
             ConnectServer();
             SendHeader();
+            client.GetStream().BeginRead(clientData.messageData, 0, clientData.messageData.Length, new AsyncCallback(ReceiveMesasage), clientData);
+            Thread sendMessageThread = new Thread(new ThreadStart (SendMessage) );
+            sendMessageThread.Start();
+
         }
 
         //서버 연결
@@ -40,32 +44,53 @@ namespace Sky_ChatClient
         //서버에 자신의 정보 헤더파일 전송
         private void SendHeader()
         {
-            string headerText = "$$#$$Header$$#$$" + clientData.clientName + "$$#$$" + clientData.testType + "$$#$$Header$$#$$";
+            string headerText = "$$#$$UserInfo$$#$$" + clientData.clientName + "$$#$$" + clientData.testType + "$$#$$UserInfo$$#$$";
             byte[] header = new byte[headerText.Length];
+            header = Encoding.UTF8.GetBytes(headerText);
             client.GetStream().Write(header, 0, header.Length);
-
         }
 
+        //서버에 자신이 나간다는 신호를 전송
         private void DisconnectServer()
         {
             string headerText = "$$#$$DisConnect$$#$$" + clientData.clientName + "$$#$$" + clientData.testType + "$$#$$DisConnect$$#$$";
             byte[] header = new byte[headerText.Length];
+            header = Encoding.UTF8.GetBytes(headerText);
             client.GetStream().Write(header, 0, header.Length);
         }
 
         //메세지 전송
-        private void SendMessage(string text)
+        private void SendMessage()
         {
-            string message = "";
-            byte[] byteData = new byte[message.Length];
-            byteData = Encoding.UTF8.GetBytes(message);
-            client.GetStream().Write(byteData, 0, byteData.Length);
+            while (true)
+            {
+                string message = "$$#$$Message$$#$$" + clientData.ipAdress + "$$#$$" + clientData.clientName + "$$#$$" + Console.ReadLine() + "$$#$$Message$$#$$";
+                Console.WriteLine("\n");
+                byte[] byteData = new byte[message.Length];
+                byteData = Encoding.UTF8.GetBytes(message);
+                client.GetStream().Write(byteData, 0, byteData.Length);
+            }
         }
 
-        //메세지를 실시간으로 받아오는 메서드
-        private void ReceiveMessage()
+        //메세지를 받아 헤더에 맞는 작업을 진행합니다.
+        private void ReceiveMesasage(IAsyncResult ar)
         {
-         
+            ClientData callbackClient = ar.AsyncState as ClientData;
+            int bytesRead = callbackClient.client.GetStream().EndRead(ar);
+            string readString = Encoding.UTF8.GetString(callbackClient.messageData, 0, bytesRead);
+            string[] messageData = readString.Split("$$#$$");
+
+            if (messageData[1] == "Message" && callbackClient.clientName != null)
+            {
+                Console.WriteLine("{0} : {1}", messageData[3], messageData[4]);
+            }
+
+            else
+            {
+            }
+
+            callbackClient.client.GetStream().BeginRead(callbackClient.messageData, 0, callbackClient.messageData.Length, new AsyncCallback(ReceiveMesasage), callbackClient);
+
         }
     }
 }
